2f32ba8269ab2af09b7c108e4fa0a170031718b5
Pedro Caio <pedroduarte@chuva-inc.com>
2025-03-26 19:20:27 -0300

    chore: Adiciona dto para response da api

---
+++ src/app/services/character-response.dto.ts
@@ -0,0 +1,11 @@
+import { Character } from "./character.interface";
+
+export interface CharacterResponse {
+  info: {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+  };
+  results: Character[];
+}
