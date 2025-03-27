18cfee4b12c340647c172f9a6a80cb776f2dc3fb
Pedro Caio <pedroduarte@chuva-inc.com>
2025-03-26 21:53:00 -0300

    chore: Adiciona documentação para rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CharacterDetailsComponent } from './character-details/character-details.component';
 
+/**
+ * Application routes configuration
+ * - Home: Main listing page
+ * - Character: Details view for individual characters
+ */
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Personagens R&M' },
   {
