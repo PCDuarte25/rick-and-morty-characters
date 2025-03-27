03bec1dbe76bdf6900247c4286aad20bc4279b6e
Pedro Caio <pedroduarte@chuva-inc.com>
2025-03-26 22:39:20 -0300

    refactor: Adiciona pipe nos componentes que recebem status

--- src/app/character-card/character-card.component.ts
+++ src/app/character-card/character-card.component.ts
@@ -2,11 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Character } from '../services/character.interface';
+import { TranslateStatusPipe } from '../shared/translate-status.pipe';
 
 @Component({
   selector: 'app-character-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TranslateStatusPipe
+  ],
   templateUrl: './character-card.component.html',
   styleUrls: ['./character-card.component.scss']
 })
@@ -15,24 +19,6 @@ export class CharacterCardComponent {
   @Input({ required: true }) character!: Character;
 
   private readonly router = inject(Router);
-
-  /**
-   * Translates status to Portuguese
-   * @param status Character status from API
-   * @returns Translated status label
-   */
-  translateStatus(status: string): string {
-    switch (status.toLowerCase()) {
-      case 'alive':
-        return 'Vivo';
-      case 'dead':
-        return 'Morto';
-      case 'unknown':
-        return 'Desconhecido';
-      default:
-        return 'Status desconhecido';
-    }
-  }
 
   /**
    * Navigates to character details page
--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -3,21 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CharacterService } from '../services/character.service';
 import { switchMap } from 'rxjs/operators';
+import { TranslateStatusPipe } from '../shared/translate-status.pipe';
 
 @Component({
   selector: 'app-character-details',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TranslateStatusPipe
+  ],
   templateUrl: './character-details.component.html',
   styleUrls: ['./character-details.component.scss']
 })
 export class CharacterDetailsComponent {
-  private route = inject(ActivatedRoute);
-  private characterService = inject(CharacterService);
+  private readonly route = inject(ActivatedRoute);
+  private readonly characterService = inject(CharacterService);
 
-  character$ = this.route.params.pipe(
+  readonly character$ = this.route.params.pipe(
     switchMap(params =>
       this.characterService.getCharacterById(Number(params['id']))
-    )
-  );
+  ));
 }
